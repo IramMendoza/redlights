b463d922ad3f6374c517d236b6bd553c80616cbb
Iram Mendoza <123773810+IramAnt@users.noreply.github.com>
2023-08-08 22:40:25 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { motion, useAnimation } from 'framer-motion'
+
+function App() {
+
+  return (
+    <>
+      <motion.div>
+        <motion.h1
+        className=' text-8xl font-bold'> REDLIGHTS </motion.h1>
+      </motion.div>
+    </>
+  )
+}
+
+export default App
